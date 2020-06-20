041bc6ec619b28e4368eeb032409912f70f9555a
lasteexile <bbriz020903@gmail.com>
2020-06-20 16:20:34 +0300

    Created base app logic, models and views

--- ex_fs/urls.py
+++ ex_fs/urls.py
@@ -23,5 +23,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('movies.urls')),
+    path('', include('motion_pictures.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
